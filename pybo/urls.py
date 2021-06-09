85fb3ccc8b718f0ece379742edf0865d737ddd97
shm <mins09649@gmail.com>
2021-06-09 15:07:03 +0900

    2-04

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index),
+
+    path('<int:question_id>/', views.detail),
+
+
+
 ]
